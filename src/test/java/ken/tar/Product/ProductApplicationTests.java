6962ffba661d218d01eef03f841d23484f92000a
KENMOEmarc <kenmarcbertrand@gmail.com>
2026-02-18 07:56:08 +0100

    Initial commit

---
+++ src/test/java/ken/tar/Product/ProductApplicationTests.java
@@ -0,0 +1,13 @@
+package ken.tar.Product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
